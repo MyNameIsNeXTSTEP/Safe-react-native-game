4a2359e300116774657a2b4ec215b7ccbf047e6f
MyNameIsNeXTSTEP <madmath@bk.ru>
2024-09-10 08:36:33 +0300

    5

--- app/components/PasswordTale.tsx
+++ app/components/PasswordTale.tsx
@@ -4,7 +4,6 @@ import { ThemedView } from './ThemedView';
 import { passwordCompareList } from '@/constants';
 import { green } from '@/constants/Colors';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import ResultPopup from './ResultPopup';
 
 const storePassword = async (value: string) => {
   try {
@@ -39,6 +38,8 @@ const PasswordInput = () => {
   const [allList, setAllList] = useState(mapList)
   const { 0: usedPasswords, 1: updateUsedPasswords } = usePasswordState();
 
+  const [loading, setLoading] = useState(false);
+
   passwordCompareList.map(pswd => {
     mapList.set(
       pswd,
@@ -53,6 +54,15 @@ const PasswordInput = () => {
       </TouchableOpacity>
     )
   });
+
+  useEffect(() => {
+    let timeoutId: number;
+    if (isPopupOpen) {
+      setLoading(true);
+      timeoutId = setTimeout(() => setLoading(false), 2000);
+    }
+    return () => clearTimeout(timeoutId);
+  }, [isPopupOpen]);
 
   useEffect(() => {
     const buttonTypeStyle = isSuccess ? styles.successButton : styles.usedButton;
@@ -92,6 +102,8 @@ const PasswordInput = () => {
     if (!pswd) return;
     setPassword(pswd);
 
+    console.log(pswd);
+
     checkIfPasswordWasUsed(pswd).then(async res => {
       try {
         if (!res) await storePassword(pswd);
@@ -118,14 +130,97 @@ const PasswordInput = () => {
           }
         </ThemedView>
       </View>
-      <ResultPopup
+
+      <div>
+        {
+          isPopupOpen
+            &&
+            <View style={stylesPopup.overlay}>
+              <ThemedView
+                style={
+                  loading
+                    ? { ...stylesPopup.container, ...stylesPopup.containerLoader }
+                    : isSuccess
+                      ? stylesPopup.container
+                      : { ...stylesPopup.container, ...stylesPopup.containerFail }
+                }
+              >
+                <p style={{ fontFamily: 'times_new_roman' }}>
+                  {
+                    loading
+                      ? 'Проверяем...'
+                      : isSuccess
+                        ? 'Unlocked ✅'
+                        : 'Пароль не верный'
+                  }
+                </p>
+                {
+                  !loading && <div
+                    className='exit-btn'
+                    style={stylesPopup.exit}
+                    onClick={() => setIsPopupOpen(false)}
+                  >
+                    <p>закрыть</p>
+                  </div>
+                }
+              </ThemedView>
+            </View>
+        }
+      </div>
+      {/* <ResultPopup
         isOpen={isPopupOpen}
         isSuccess={isSuccess}
         setIsPopupOpen={setIsPopupOpen}
-      />
+      /> */}
     </View>
   );
 };
+
+const stylesPopup = StyleSheet.create({
+  overlay: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    zIndex: 3,
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: 'rgba(0,0,0,0.5)',
+  },
+  container: {
+    width: 300,
+    height: 200,
+    borderRadius: 25,
+    backgroundColor: 'green',
+    position: 'relative',
+    alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 'auto',
+    color: 'white',
+  },
+  containerFail: {
+    backgroundColor: 'red',
+  },
+  containerLoader: {
+    backgroundColor: 'orange',
+  },
+  loader: {
+    position: 'relative',
+    alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 'auto',
+  },
+  exit: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    cursor: 'pointer',
+    marginRight: 12,
+  },
+});
 
 const styles = StyleSheet.create({
   safe: {
